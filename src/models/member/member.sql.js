34602f196cf81bc111c9be82ff86f901e2a5804b
JJHyeok13 <jjinh99@naver.com>
2024-06-03 12:15:28 +0900

    feat: SQL문 주석 추가

--- src/models/member/member.sql.js
+++ src/models/member/member.sql.js
@@ -1,19 +1,24 @@
+// Member 추가하기
 export const insertMemberSql = `
   INSERT INTO Member (name, gender, birthday, addr, specAddr, phone, email, social_type, point) VALUES (?, ?, ?, ?, ?, ?, ?, ?, 0)
 `;
 
+// ID 값으로 Member 찾기
 export const getMemberByID = `
   SELECT * FROM Member WHERE id = ?
 `;
 
+// Member와 FoodType 연결하기 (MemberFoodType Table에 추가하기)
 export const connectFoodCategory = `
   INSERT INTO MemberFoodType (member_id, foodtype_id) VALUES (?, ?);
 `;
 
+// 중복되는 이메일 확인하기
 export const confirmEmail = `
   SELECT EXISTS(SELECT 1 FROM Member WHERE email = ?) as isExistEmail;
 `;
 
+//
 export const getPreferToMemberID = `
   SELECT mft.id, mft.foodtype_id, mft.member_id, ft.name
   FROM MemberFoodType mft
--- src/models/restaurant/restaurant.sql.js
+++ src/models/restaurant/restaurant.sql.js
@@ -1,7 +1,10 @@
+// ID 값으로 Restaurant 찾기
 export const getRestaurantById = `SELECT * FROM Restaurant WHERE id = ?`;
 
+// Restaurant 추가하기
 export const insertRestaurant = `INSERT INTO Restaurant (name, address, region_id, foodtype_id) VALUES (?, ?, ?, ?)`;
 
+// 중복되는 식당 확인하기
 export const confirmRestaurant = `
     SELECT EXISTS (
         SELECT 1
@@ -11,6 +14,8 @@ export const confirmRestaurant = `
     ) AS isExistRestaurant;
 `;
 
+// ID 값으로 Review 찾기
 export const getReviewById = `SELECT * FROM Review WHERE id = ?`;
 
-export const insertReview = `INSERT INTO Review (member_id, restaurant_id, content, rating) VALUES (?, ?, ?, ?)`;
+// Review 추가하기
+export const insertReview = `INSERT INTO Review (member_id, restaurant_id, rating, content) VALUES (?, ?, ?, ?)`;
